a1d1b74d96245d407d02b9e205e806040bca3aee
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2024-03-12 02:09:41 +0200

    видаляє коментар з store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// // import { persistStore } from 'redux-persist';
-// import persistStore from 'redux-persist/es/persistStore';
-// import contactsReducer from './contactsSlice';
-// // import { getDefaultMiddleware } from '@reduxjs/toolkit';
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//   },
-//   // middleware: getDefaultMiddleware({
-//   //   serializableCheck: false,
-//   // }),
-// });
-
-// export const persistor = persistStore(store);
-
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
